5e7f8304e186fea478474a47ba1112e933262c86
Timur Kaftaranov <kaftaranov@rambler.ru>
2022-05-10 15:41:11 +0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        int year = 2148;
+        if (isLeapYear(year)) {
+            System.out.println(year + " is leap.");}
+        else System.out.println(year + " is not leap.");
+    }
+    public static boolean isLeapYear(int year)
+    {
+        return year % 4 == 0 && year % 100 == 0 && year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
     }
 
 }
